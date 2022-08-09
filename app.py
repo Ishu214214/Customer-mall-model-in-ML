7ae1f4fadfc053df9069bae75ee7ac1cda8b77f7
Ishu214214 <83811024+Ishu214214@users.noreply.github.com>
2022-08-09 12:55:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,6 @@ def predict():
   return render_template('index.html', prediction_text='Model  has predicted  : {}'.format(result))
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
 
-app.run()
